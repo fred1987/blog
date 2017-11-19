a0d7cfbb2396cdbdaeacbca5c10f63c5ef05e2ba
Alfred <hi@ifred.ru>
2017-11-19 21:23:03 +0300

    Revert "ДЗ8"

--- resources/views/widgets/right_menu.blade.php
+++ resources/views/widgets/right_menu.blade.php
@@ -1,13 +1,15 @@
 @section('right_menu')
-    <div class="widget-categories  push-down-30">
-        <h6>РАЗДЕЛЫ</h6>
-        <ul>
-            @foreach($articleSections as $elem)
-                <li>
-                    <a href="/?section={{ $elem['slug'] }}">{{ $elem['name'] }} <span
-                                class="widget-categories__text">{{ $elem['count'] }}</span></a>
-                </li>
-            @endforeach
-        </ul>
-    </div>
+    @if(count($articleSections) >0)
+        <div class="widget-categories  push-down-30">
+            <h6>РАЗДЕЛЫ</h6>
+            <ul>
+                @foreach($articleSections as $elem)
+                    <li>
+                        <a href="/?section={{ $elem['slug'] }}">{{ $elem['name'] }} <span
+                                    class="widget-categories__text">{{ $elem['count'] }}</span></a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 @show
--- resources/views/widgets/tags.blade.php
+++ resources/views/widgets/tags.blade.php
@@ -1,9 +1,11 @@
 @section('tags')
-    <div class="tags  widget-tags">
-        <h6>Тэги</h6>
-        <hr>
-        @foreach($tags as $tag)
-            <a href="/?tag={{ $tag['slug'] }}" class="tags__link">{{ $tag['name'] }}</a>
-        @endforeach
-    </div>
+    @if(count($tags) >0)
+        <div class="tags  widget-tags">
+            <h6>Тэги</h6>
+            <hr>
+            @foreach($tags as $tag)
+                <a href="/?tag={{ $tag['slug'] }}" class="tags__link">{{ $tag['name'] }}</a>
+            @endforeach
+        </div>
+    @endif
 @show
